5d2b27e2b54dac45ad28175927b60baa09a6fbd8
OneGrit <2012joe@gmail.com>
2019-03-28 11:07:46 +0800

    fix owner Foreig_key on_delete arguments

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='名称')
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
     is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者",on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
