1f57cee5ea60389955873bd47bbb202548ed065a
OneGrit <2012joe@gmail.com>
2019-03-28 11:20:41 +0800

    created db tables

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='名称')
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
     is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
-    owner = models.ForeignKey(User, verbose_name="作者",on_delete=models.DO_NOTHING)
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
@@ -30,7 +30,7 @@ class Tag(models.Model):
 
     name = models.CharField(max_length=128, verbose_name="名称")
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
-    owner = models.ForeignKey(User, verbose_name="作者")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
@@ -51,9 +51,9 @@ class Post(models.Model):
     desc = models.TextField(max_length=1024, blank=True, verbose_name='摘要')
     content = models.TextField(verbose_name='正文', help_text='正文可以为Markdown格式')
     status = models.PositiveIntegerField(default=STATUS_DRAFT, verbose_name='状态', choices=STATUS_ITEMS)
-    category = models.ForeignKey(Category, verbose_name='分类')
-    tag = models.ForeignKey(Tag, verbose_name='标签')
-    owner = models.ForeignKey(User, verbose_name='作者')
+    category = models.ForeignKey(Category, verbose_name='分类',on_delete=models.DO_NOTHING)
+    tag = models.ForeignKey(Tag, verbose_name='标签',on_delete=models.DO_NOTHING)
+    owner = models.ForeignKey(User, verbose_name='作者',on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     class Meta:
